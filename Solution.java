cc16ba5c109ea050ef0fdcbcf7981be7a8795d9b
MonicaMM24 <monica.mager123@gmail.com>
2025-07-29 17:17:40 +0300

    DayOne Solution

--- Solution.java
+++ Solution.java
@@ -1,28 +1,27 @@
+
 import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        // Given (already initialized in HackerRank editor)
+
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
 
-        // Step 1: Declare your variables
         int userInt;
         double userDouble;
         String userString;
 
-        try (// Step 2: Read input
+        try (
         Scanner scanner = new Scanner(System.in)) {
-            userInt = scanner.nextInt(); // Read int
-            userDouble = scanner.nextDouble(); // Read double
-            scanner.nextLine(); // Consume newline
-            userString = scanner.nextLine(); // Read full line of text
+            userInt = scanner.nextInt(); 
+            userDouble = scanner.nextDouble(); 
+            scanner.nextLine(); 
+            userString = scanner.nextLine(); 
 
-            // Step 3: Perform operations and print
-            System.out.println(i + userInt); // int sum
-            System.out.printf("%.1f\n", d + userDouble); // double sum, 1 decimal place
-            System.out.println(s + userString); // string concatenation
+            System.out.println(i + userInt); 
+            System.out.printf("%.1f\n", d + userDouble); 
+            System.out.println(s + userString); 
 
             scanner.close();
         }
